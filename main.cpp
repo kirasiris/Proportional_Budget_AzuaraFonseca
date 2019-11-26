93522e5f8cd770117b134d434650a7280676ffa3
Kevin Uriel Fonseca <kebin1421@hotmail.com>
2019-11-26 23:16:37 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class Person {
+  private:
+    double paycheckJobOne = 6250.00;
+    double paycheckOneJobOne = 500.00;
+    double paycheckTwoJobTwo = 650.00;
+    double TIPS = 0.0;
+  public:
+};
+
+int main() {
+  std::cout << "Hello World!\n";
+}
